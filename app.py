92a08edd5e67c61eeff8f38faa63ceae72ef8183
Travis Jackson-McFarland <jacksonmcfarland14@gmail.com>
2025-12-02 14:39:13 -0500

    Update league_id secret key reference

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ st.title("🏈 Fantasy League Recap")
 
 try:
     # Load secrets
-    league_id = st.secrets["644655941"]
+    league_id = st.secrets["league_id"]
     swid = st.secrets["318A390B-4F7D-43EA-ACF3-3CDC06A36F9F"]
     espn_s2 = st.secrets["AEAqA4VyoKfmfOGj%2FgFtpq2WY6%2B%2Bu9HgogCc1%2FO8ze9ClTKjI%2FlvVTlBLzeAlROL4D%2FZ4CrLSambNQ1uCK9kvhbPQaBjpEDLctH2wUrc3%2Fy%2FzMYArr4drMAjcKa0pSSPBVcI%2BchNJvAPovUqonXhUAKByw958m8SE6x99qYJJEgfOOFJ%2BKenVOt2Xw%2FYDlQ1qmy%2FJL1NPtJf06%2B45Vie6ZpNrL8LuzZCyM16sLO62bAszJ07U1JfNS%2FJ%2BUztSLMBIfpuUBGZ4KJLvnpqVoQxi4%2Fg8jv5aF6d4VrgLfVV1%2BbzNnHsd2sRVO09LMW%2BUR7QU5k%3D"]
     
