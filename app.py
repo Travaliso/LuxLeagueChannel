8ceef68cb19e7b2003281a5ff04b7d18d6f1f779
Travis Jackson-McFarland <jacksonmcfarland14@gmail.com>
2026-09-10 08:34:04 -0400

    Fix indentation in box_scores data retrieval

--- app.py
+++ app.py
@@ -107,12 +107,12 @@ with st.sidebar:
 if 'box_scores' not in st.session_state or st.session_state.get('week') != selected_week:
     with ui.luxury_spinner(f"Accessing Week {selected_week} Data..."):
         try:
-    st.session_state['box_scores'] = league.box_scores(week=selected_week)
-except Exception:
-    st.session_state['box_scores'] = []
-    st.warning("Matchup data is not yet available from ESPN. Box scores will populate closer to kickoff.")
-        st.session_state['week'] = selected_week
-    st.rerun()
+        st.session_state['box_scores'] = league.box_scores(week=selected_week)
+    except Exception:
+        st.session_state['box_scores'] = []
+        st.warning("Matchup data is not yet available from ESPN. Box scores will populate closer to kickoff.")
+            st.session_state['week'] = selected_week
+        st.rerun()
 
 box_scores = st.session_state['box_scores']
 
